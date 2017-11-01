80c6bcea533ec2d303de6db49c04d6c1cd718e1a
Chase Paules <cpaules@gmail.com>
2017-11-01 20:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getRepositories() {
   const req = new XMLHttpRequest()
   const username = document.getElementById('username').value
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
 
