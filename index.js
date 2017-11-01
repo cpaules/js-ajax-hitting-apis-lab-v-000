98960b4ad9c9724e3121dbd59d4bf72a07d68f9a
Chase Paules <cpaules@gmail.com>
2017-11-01 21:05:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function displayRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList
 }
 
-function getCommits() {
-  
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
 }
