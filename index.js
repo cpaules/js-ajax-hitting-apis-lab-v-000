77c3b89d91fb74a7efc0e72072f6f4debb38ae10
Chase Paules <cpaules@gmail.com>
2017-11-01 20:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
   const username = document.getElementById('username').value
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
 
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   let repos = JSON.parse(this.responseText)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
