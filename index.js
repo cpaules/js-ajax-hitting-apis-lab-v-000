c7b16812c4b30766bd4d67ff550a5635c9a591f4
Chase Paules <cpaules@gmail.com>
2017-11-01 21:15:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getRepositories() {
 
 function displayRepositories(event, data) {
   let repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li>' + '<h3>' + r.username + '</h3>' + r.html_url + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + '<h3>' + r.name + '</h3>' + r.html_url + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
