c4dfe279157000745a3e385dbe13f13af6df9081
Chase Paules <cpaules@gmail.com>
2017-11-01 21:03:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,34 +11,3 @@ function displayRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' + '<h3>' + r.name + '</h3>' + r.html_url + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
-
-function getCommits(el) {
-  const repoName = el.dataset.repository
-  const uri = "https://api.github.com/repos/" + el.dataset.username + "/" + repoName + "/commits"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayCommits)
-  xhr.open("GET", uri)
-  xhr.send()
-}
-
-function displayCommits() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name +
-' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("details").innerHTML = commitsList
-}
-
-function getBranches(el) {
-  const repoName = el.dataset.repository
-  const uri = "https://api.github.com" + "/repos/" + el.dataset.username + "/" + repoName + "/branches"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayBranches)
-  xhr.open("GET", uri)
-  xhr.send()
-}
-
-function displayBranches() {
-  const branches = JSON.parse(this.responseText)
-  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
-  document.getElementById("details").innerHTML = branchesList
-}
