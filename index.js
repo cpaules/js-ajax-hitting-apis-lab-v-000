954142396b0925dbe5b53985e6b20e7a8ab6a63e
Chase Paules <cpaules@gmail.com>
2017-11-01 21:32:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function displayRepositories(event, data) {
 }
 
 function getCommits(el) {
-  console.log(el.dataset.repo.login)
+  console.log(el)
   const repoName = el.dataset.repo
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
