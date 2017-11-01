fc3b80cf5008f9c53772311476117d9df0b72135
Chase Paules <cpaules@gmail.com>
2017-11-01 21:58:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function getBranches(el) {
 }
 
 function displayBranches() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => '<li><strong>' + branch.author.login + '</strong> - ' + branch.branch.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
